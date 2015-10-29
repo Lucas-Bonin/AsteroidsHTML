742b77f2625482d04d43a137a71bbc53fce8ab01
Lucas Bonin <lucasbonin369@gmail.com>
2015-10-29 11:19:27 -0200

    correções

--- Asteroids/js/menuState.js
+++ Asteroids/js/menuState.js
@@ -1,7 +1,7 @@
 var MenuState = State.extend({
 
 	init: function(game){ //quando iniciar menu, respawnar alguns asteroides no background
-		this.super(game);
+		this._super(game);
 
 		this.canvasWidth = game.canvas.ctx.width;
 		this.canvasHeight = game.canvas.ctx.height;
